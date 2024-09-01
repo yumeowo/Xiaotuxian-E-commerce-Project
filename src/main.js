bb3ed63226d200fc99a3af24feb35524d89c53e9
Yume <2839681263@qq.com>
2024-09-01 14:49:52 +0800

    *删除部分测试

--- src/main.js
+++ src/main.js
@@ -7,15 +7,6 @@ import router from './router'
 // 引入初始化样式文件
 import '@/styles/common.scss'
 
-// 测试接口函数
-// import { getCategory } from '@/apis/testAPI'
-// getCategory().then((result) => {
-//   console.log(result)
-// }).catch((err) => {
-//   console.log(err)
-//   console.log("接口函数有误！")
-// })
-
 const app = createApp(App)
 
 app.use(createPinia())
