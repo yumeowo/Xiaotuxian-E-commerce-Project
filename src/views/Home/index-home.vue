89709092a4bba293e0ff720c018b8f9be144017e
Yume <2839681263@qq.com>
2024-09-02 17:09:28 +0800

    Home-面板组件封装

--- src/views/Home/index-home.vue
+++ src/views/Home/index-home.vue
@@ -3,6 +3,7 @@ import HomeBanner from './components/HomeBanner.vue';
 import HomeCategory from './components/HomeCategory.vue';
 import HomeHot from './components/HomeHot.vue';
 import HomeNew from './components/HomeNew.vue';
+import HomePanel from './components/HomePanel.vue';
 import HomeProduct from './components/HomeProduct.vue';
 
 
@@ -16,4 +17,13 @@ import HomeProduct from './components/HomeProduct.vue';
   <HomeNew />
   <HomeHot />
   <HomeProduct />
+
+  <HomePanel title="新鲜好物" sub-title="好物推荐 商品...">
+    <div>
+      测试
+    </div>
+  </HomePanel>
+  <HomePanel title="人气推荐">
+
+  </HomePanel>
 </template>
